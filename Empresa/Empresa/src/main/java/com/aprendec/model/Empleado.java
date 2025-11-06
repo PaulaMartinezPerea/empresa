002636cff6273a72e8cbc66a31d5dda261d73dc8
Paula Martinez Perea <pmarper2003@g.educaand.es>
2025-11-06 13:31:54 +0100

    Add files via upload

---
+++ Empresa/Empresa/src/main/java/com/aprendec/model/Empleado.java
@@ -0,0 +1,29 @@
+package com.aprendec.model;
+
+
+public class Empleado {
+    private String dni;
+    private String nombre;
+    private char sexo;  
+    private int categoria;
+    private int anyos;
+    
+
+    public String getDni() { return dni; }
+    public void setDni(String dni) { this.dni = dni; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public char getSexo() { return sexo; }
+    public void setSexo(char sexo) { this.sexo = sexo; }
+
+    public int getCategoria() { return categoria; }
+    public void setCategoria(int categoria) { this.categoria = categoria; }
+
+    public int getAniosTrabajados() { return anyos; }
+    public void setAniosTrabajados(int anyos) { this.anyos = anyos; }
+
+
+}
+
